41b62a6c8b29d00fd2d39b2dad8e3db286999fd8
Dudaram Choudhary <dudsoffice656@gmail.com>
2025-07-30 19:12:35 +0530

    minor changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,31 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
+    console.log("Register endpoint hit");
+    console.log("Request body:", req.body);
+
+    if (!req.body) {
+        return res.status(400).json({
+            success: false,
+            message: "Request body missing or not parsed. Please send application/json data."
+        });
+    }
+
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).json({
+            success: false,
+            message: "Email is required"
+        });
+    }
+
+    console.log("Dudaram Choudhary", email);
+
     res.status(200).json({
-        message: "Hello Dudaram "
+        success: true,
+        message: "User registered",
+        data: { email }
     });
 });
 
